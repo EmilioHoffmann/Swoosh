815b89f73e0e29c63e124a5790c4b82d7653975b
EmilioHoffmann <emiliohoffmann@hotmail.com>
2018-09-23 17:52:33 -0300

    Handling clicks on leagues layout

---
+++ app/src/main/java/com/example/emili/swoosh/Constants.kt
@@ -0,0 +1,3 @@
+package com.example.emili.swoosh
+
+const val EXTRA_LEAGUE = "EXTRA_LEAGUE"
--- app/src/main/java/com/example/emili/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/emili/swoosh/LeagueActivity.kt
@@ -3,16 +3,43 @@ package com.example.emili.swoosh
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
 
+    var selectedLeague = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
 
-    fun leagueNextClicked(view: View){
-        val intent = Intent(this, SkillActivity::class.java)
-        startActivity(intent)
+    fun onMensClicked(view: View) {
+        womensLeagueButton.isChecked = false
+        coedLeagueButton.isChecked = false
+        selectedLeague = "mens"
     }
+
+    fun onWomensClicked(view: View) {
+        mensLeagueButton.isChecked = false
+        coedLeagueButton.isChecked = false
+        selectedLeague = "womens"
+    }
+
+    fun onCoedClicked(view: View) {
+        womensLeagueButton.isChecked = false
+        mensLeagueButton.isChecked = false
+        selectedLeague = "co-ed"
+    }
+
+    fun leagueNextClicked(view: View) {
+        if(selectedLeague != "" && mensLeagueButton.isChecked || womensLeagueButton.isChecked || coedLeagueButton.isChecked) {
+            val intent = Intent(this, SkillActivity::class.java)
+            intent.putExtra(EXTRA_LEAGUE, selectedLeague)
+            startActivity(intent)
+        }else{
+            Toast.makeText(this, "Please select a league", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
