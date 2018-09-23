c04ab3f33b1bd649e2b548abb3a9f68b2c98cd69
EmilioHoffmann <emiliohoffmann@hotmail.com>
2018-09-23 17:32:02 -0300

    Skill activity layout created

--- app/src/main/java/com/example/emili/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/example/emili/swoosh/WelcomeActivity.kt
@@ -1,12 +1,19 @@
 package com.example.emili.swoosh
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
 
-class WelcomeActivity : AppCompatActivity() {
+class WelcomeActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        get_started_button.setOnClickListener {
+
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
